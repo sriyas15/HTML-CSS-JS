2d041ebfc351e41eaf19029caa9b2a9f5b9a231b
Mohamed Riyas <sriyas1508@gmail.com>
2025-08-14 20:03:17 +0530

    updated cart stuffs

--- Amazon Project/data/cart.js
+++ Amazon Project/data/cart.js
@@ -2,6 +2,10 @@
         productId:"901eb2ca-386d-432e-82f0-6fb1ee7bf969",
         quantity:1
     },
+    {
+        productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity:2
+    }
 ];
 
 export function addToCart(productName,productId){
